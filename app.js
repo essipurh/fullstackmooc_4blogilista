17d95426bab3cb4657b36497db38301a7d5da7d8
Essi Purhonen <pessiapps@gmail.com>
2024-06-27 11:45:34 +0300

    logger lisätty, 4.2

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { info } = require('./utils/logger')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 
 mongoose.set('strictQuery', false) // tallentaa db:ghen myös muut kentät requestin mukana, ei vain schemassa määritellyt. kts. https://www.mongodb.com/community/forums/t/deprecationwarning-mongoose-the-strictquery/209637
-console.log('*** Connecting to:', config.MONGODB_URI)
+info('*** Connecting to:', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(error => console.log('ERROR connecting to mongodb', error.message))
+  .then(() => info('Connected to MongoDB'))
+  .catch(error => info('ERROR connecting to mongodb', error.message))
 
 app.use(cors())
 app.use(express.json())
